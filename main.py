9cf13c11aca89d80f755b66b9365093598204900
Leonardo <leodparaujo02@gmail.com>
2025-12-10 16:06:50 -0300

    teste

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Ola, Git e Github!")
+login_salvo = "admin_ti"
+senha_salva = "Sistema_123"
+
+
+login_digitado = input (f"Digite seu login:")
+senha_digitada = input (f"digite sua senha:")
+
+if login_digitado == login_salvo and senha_digitada == senha_salva:
+    print(f"Bem-vindo ao painel de controle")
+elif login_digitado != login_salvo  or senha_digitada != senha_salva:
+    print("Acesso Negado: Credenciais de baixo riso ou padrão de segurança.")
+else:
+    print("Erro de Acesso: login ou senha inválidos")
