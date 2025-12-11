d1bbcbe86477bb5183c6c326abac699654b189d8
Leonardo <leodparaujo02@gmail.com>
2025-12-11 13:32:56 -0300

    Gerador de Tabuada

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-login_salvo = "admin_ti"
-senha_salva = "Sistema_123"
 
+numero = int(input("Digite um número inteiro para ver sua tabuada: "))
 
-login_digitado = input (f"Digite seu login:")
-senha_digitada = input (f"digite sua senha:")
-
-if login_digitado == login_salvo and senha_digitada == senha_salva:
-    print(f"Bem-vindo ao painel de controle")
-elif login_digitado != login_salvo  or senha_digitada != senha_salva:
-    print("Acesso Negado: Credenciais de baixo riso ou padrão de segurança.")
-else:
-    print("Erro de Acesso: Login ou senha inválidos!")
+for i in range(1, 10 + 1):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
 
